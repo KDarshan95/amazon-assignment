e245f272a062a087b57909ba3e71aa55005bcafd
Darshan K <darshankhope@Darshans-MacBook-Air-2.local>
2025-01-28 22:37:33 +0530

    test(amazon): Update search test log

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -88,6 +88,6 @@ test.describe("Amazon test", () => {
       const productTitle = await watchPage.getProductTitle();
       expect(productTitle).toContain("Starlight");
     });
-    console.log(`Amazon Product Iphone 13 128gb search test has been successfully executed`);
+    console.log(`Amazon Product Iphone 13 128gb from Apple search test has been successfully executed`);
   });
 });
